ae471555956beee247324e4b5619535eb05126a9
abhi-061 <abhi22umachigi@gmail.com>
2023-08-19 12:08:39 +0530

    Added vars and provider files

---
+++ vars.tf
@@ -0,0 +1,95 @@
+variable REGION {
+  default = "us-east-1"
+}
+
+variable AMIS {
+  type=map
+  default = {
+    us-east-1 = ami-0261755bbcb8c4a84
+  }
+}
+
+variable PRIV_KEY_PATH {
+  default = " "
+}
+
+variable PUB_KEY_PATH {
+  default = "vprofilekey.pub"
+}
+
+variable USERNAME {
+  default = "ubuntu"
+}
+
+variable MYIP {
+  default = "49.37.182.21/32"
+}
+
+variable rmquser {
+  default = "rabbit"
+}
+
+variable rmqpass {
+  default = "admin@987654"
+}
+
+variable dbname {
+  default = "accounts"
+}
+
+variable dbuser {
+  default = "admin"
+}
+
+variable dbpass {
+  default = "admin123"
+}
+
+variable instance_count {
+  default = "1"
+}
+
+variable VPC_NAME {
+  default = "vpro_vpc"
+}
+
+variable ZONE1 {
+  default = "us-east-1a"
+}
+
+variable ZONE2 {
+  default = "us-east-1b"
+}
+
+variable ZONE3 {
+  default = "us-east-1c"
+}
+
+variable vpcCIDR {
+  default = "172.21.0.0/16"
+}
+
+variable PubSub1CIDR {
+  default = "172.21.1.0/24"
+}
+
+variable PubSub2CIDR {
+  default = "172.21.2.0/24"
+}
+
+variable PubSub3CIDR {
+  default = "172.21.3.0/24"
+}
+
+variable PrivSub1CIDR {
+  default = "172.21.4.0/24"
+}
+
+variable PrivSub2CIDR {
+  default = "172.21.5.0/24"
+}
+
+variable PrivSub3CIDR {
+  default = "172.21.6.0/24"
+}
+
